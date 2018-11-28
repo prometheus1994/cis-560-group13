38cfdc91aaf63dfa391f6114a85e64e47f126729
Bill Van Cleve <vancleve@ksu.edu>
2018-11-28 17:50:08 -0600

    Create account window fixes

--- PhilsRentals/PhilsRentals/Views/CreateAccountWindow.cs
+++ PhilsRentals/PhilsRentals/Views/CreateAccountWindow.cs
@@ -21,25 +21,18 @@ namespace PhilsRentals.Views
             InitializeComponent();
             
         }
-
-
-
         private void uxButtonAccept_Click(object sender, EventArgs e)
         {
-            if (uxTextboxFirstName.Text == "" || uxTextboxLastName.Text == "" || uxTextboxPhoneNumber.Text == "" || uxTextboxEmail.Text == "")
-            {
-                MessageBox.Show("Error: Please Make Sure All Fields Are Filled");
-            }
-            else
-            {
-                firstName = uxTextboxFirstName.Text;
-                lastName = uxTextboxLastName.Text;
-                email = uxTextboxEmail.Text;
-                phoneNumber = String.Format("{0:(###) ###-####}", Convert.ToDouble(uxTextboxPhoneNumber.Text));
-                MessageBox.Show(phoneNumber);
+            firstName = uxTextboxFirstName.Text;
+            lastName = uxTextboxLastName.Text;
+            email = uxTextboxEmail.Text;
+            phoneNumber = String.Format("{0:(###) ###-####}", Convert.ToDouble(uxTextboxPhoneNumber.Text));
+            MessageBox.Show(phoneNumber);
+        }
 
-                
-            }
+        private void uxTextboxTextChanged(object sender, EventArgs e)
+        {
+            uxButtonAccept.Enabled = uxTextboxEmail.TextLength > 0 && uxTextboxFirstName.TextLength > 0 && uxTextboxLastName.TextLength > 0 && uxTextboxPhoneNumber.TextLength > 9;
         }
     }
 }
