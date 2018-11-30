8a8ae82a296db156223619e45640adb0aa487ed8
Joseph Webster <joseph.eli.webster@gmail.com>
2018-11-30 11:17:13 -0600

    Added the GetAccountInformation() method to MainWindowController.

--- PhilsRentals/PhilsRentals/MainWindowController.cs
+++ PhilsRentals/PhilsRentals/MainWindowController.cs
@@ -4,11 +4,19 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Data;
+using System.Data.SqlClient;
 
 namespace PhilsRentals
 {
     public class MainWindowController
     {
+
+        /// <summary>
+        /// Connection string for accessing the database.
+        /// </summary>
+        private const string _connection = "Server=[server_name];Database=[group13proj];Trusted_Connection=true";
+
         /// <summary>
         /// Handle to the MainWindow which is the view for this controller.
         /// </summary>
@@ -44,6 +52,10 @@ namespace PhilsRentals
             /* Code to send window to main window */
             IWindow window;
             _windows.TryGetValue(operation, out window);
+
+            // modAcctLookup for ModifyAccountWindow.cs
+            List<string> account_info = GetAccountInformation("Put Email Here");
+
             return window;
 
             // Create a switch for the passed in argument which contains any operation.
@@ -51,5 +63,52 @@ namespace PhilsRentals
         }
 
         // Create private functions for each different operation.
+
+        /// <summary>
+        /// Gets the Account Info using stored procedure ""
+        /// </summary>
+        /// <param name="email"></param>
+        private List<string> GetAccountInformation(string email)
+        {
+            List<string> account_info = new List<string>();
+            using (SqlConnection conn = new SqlConnection())
+            {
+
+                conn.ConnectionString = _connection;
+
+                // procedure to grab the account info
+                SqlCommand cmd = new SqlCommand("modAcctLookup", conn);
+                cmd.CommandType = System.Data.CommandType.StoredProcedure;
+
+                SqlParameter param = new SqlParameter();
+                param.ParameterName = "Email";
+                param.Value = email; // Get Value
+                cmd.Parameters.Add(param);
+
+                conn.Open();
+                int row_check = cmd.ExecuteNonQuery();
+                
+                // if the number of rows returned is not 1, then the SQL procedure failed.
+                // we only need to grab 1 row because one row represents 1 account.
+                if (row_check != 1)
+                {
+                    throw new Exception("Invalid Procedure Call");
+                }
+
+                using (SqlDataReader reader = cmd.ExecuteReader())
+                {
+                    if (reader.Read())
+                    {
+                        account_info.Add(reader["Email"].ToString());
+                        account_info.Add(reader["PhoneNumber"].ToString());
+                        account_info.Add(reader["FirstName"].ToString());
+                        account_info.Add(reader["LastName"].ToString());
+                    }
+                }
+                conn.Close();
+            }
+
+            return account_info;
+        }
     }
 }
--- PhilsRentals/PhilsRentals/Views/ModifyAccountWindow.cs
+++ PhilsRentals/PhilsRentals/Views/ModifyAccountWindow.cs
@@ -7,6 +7,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using System.Data.SqlClient;
 
 namespace PhilsRentals.Views
 {
@@ -30,25 +31,49 @@ namespace PhilsRentals.Views
             }
         }
 
+
+        /// <summary>
+        /// Pulls the customer info from the SQL Tables
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
         private void uxButtonUpdateInfo_Click(object sender, EventArgs e)
         {
             throw new NotImplementedException();
         }
 
+        /* 
+         * create table group13proj.Account
+         * 
+         * (
+	            AccountID int not null identity(1,1) primary key,
+	            FirstName NVARCHAR(32) not null,
+	            LastName NVARCHAR(32) not null,
+	            PhoneNumber NVARCHAR(15) not null,
+	            Email NVARCHAR(64) not null unique
+            )
+         */
+        /// <summary>
+        /// Looks for the email in the Account table
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
         private void uxButtonSearchEmail_Click(object sender, EventArgs e)
         {
+            // [0] = AccountID
+            // [1] = FirstName
+            // ...
+            // [4] = Email
+            string[] account_info = new string[5];
+
             uxTextBoxFirstName.Enabled = true;
             uxTextBoxLastName.Enabled = true;
             uxTextBoxNewEmail.Enabled = true;
             uxTextBoxNewEmail.Text = uxTextBoxSearchEmail.Text;
             uxTextBoxPhoneNumber.Enabled = true;
-
-            uxTextBoxNewEmail.Text = uxTextBoxSearchEmail.Text;
-        }
-
-        private void uxButtonUpdateInfo_Click(object sender, EventArgs e)
-        {
-
+            // grab the existing email from the TextBoxSearchEmail
+            // uxTextBoxNewEmail.Text = uxTextBoxSearchEmail.Text;
         }
     }
 }
+ 
