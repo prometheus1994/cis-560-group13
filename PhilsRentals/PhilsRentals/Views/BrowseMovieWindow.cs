6ce4121043ee4bcdd304cfbb0f7afed82b39460e
Webster <31218426+prometheus1994@users.noreply.github.com>
2018-12-04 00:30:55 -0500

    Jwebster7 edit browse movie ui (#22)
    * stored the values of a browse movie querry in fields.
    
    * Add FormatData() to BrowseMovieWindow.cs.
    
    Called _mwc.GetMovies() with the parameters for filtering the movie data.

--- PhilsRentals/PhilsRentals/Views/BrowseMovieWindow.cs
+++ PhilsRentals/PhilsRentals/Views/BrowseMovieWindow.cs
@@ -22,6 +22,36 @@ namespace PhilsRentals.Views
         /// </summary>
         IMainWindowController _mwc;
 
+        /// <summary>
+        /// The title of the movie if they know it
+        /// </summary>
+        private string _title = "";
+
+        /// <summary>
+        /// The genre from the checked list box
+        /// </summary>
+        private List<string> _genres = new List<string>();
+
+        /// <summary>
+        /// The compiled genres
+        /// </summary>
+        private string _compiledGenres = "";
+
+        /// <summary>
+        /// The rating of the movie 
+        /// </summary>
+        private double _rating = 0;
+
+        /// <summary>
+        /// The year the movie came out
+        /// </summary>
+        private int _year = 0;
+
+        /// <summary>
+        /// The duration/length of the movie in minutes
+        /// </summary>
+        private double _length = 0;
+
         public BrowseMovieWindow(IMainWindowController mwc)
         {
             InitializeComponent();
@@ -30,20 +60,74 @@ namespace PhilsRentals.Views
 
         private void uxTextBoxTextChanged(object sender, EventArgs e)
         {
+            // need to add optional conditions
             uxButtonGetMovie.Enabled = uxTextBoxMovieTitle.TextLength > 0;
         }
 
+        /// <summary>
+        /// References StoredProcedures -> allFilters
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
         private void uxButtonGetMovie_Click(object sender, EventArgs e)
         {
-            uxTextBoxMovieTitle.Text = "";
+            // Need to call functions/stored procedures to filter movies
+            // by optional parameters.
+            _title = uxTextBoxMovieTitle.Text;
+            foreach(var item in uxCheckedListBoxMovieGenre.CheckedItems)
+            {
+                _genres.Add(item.ToString());
+            }
+            _rating = Convert.ToInt32(uxNumericUpDownRating.Value);
+            _year = Convert.ToInt32(uxNumericUpDownYear.Value);
+            _length = Convert.ToInt32(uxNumericUpDownDuration.Value);
+            
+            FormatData();
+            
+            
         }
 
         private bool GetMovies()
         {
+
             return false;
         }
 
+        private void FormatData()
+        {
+            if (String.IsNullOrEmpty(_title))
+            {
+                _title = null;
+            }
+            if (_genres.Count == 0)
+            {
+                _genres = null;
+            }
+            if (_year < 1888)
+            {
+                _year = 0;// movies weren't made before 1888
+            }
+            if (_length / 60 < 100)
+            {
+                _length = 0;//the shortest movie in the world is 100 seconds, _length is in minutes
+            }
+
+            // joins all strings from _genres into a single, compiled string seperated by commas
+            _compiledGenres = String.Join(",", _genres);
+
+            // pass the values via GetMovies()?
+            _mwc.GetMovies(_title, _compiledGenres, _year, '0', Convert.ToInt32(_length), '0', (float)_rating);
+
+        }
+
+
+        /* Can disregard these methods */
         private void uxListBoxMovies_DoubleClick(object sender, EventArgs e)
+        {
+
+        }
+
+        private void uxCheckedListBoxMovieGenre_SelectedIndexChanged(object sender, EventArgs e)
         {
 
         }
