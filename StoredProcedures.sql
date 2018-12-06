e02ae415fd5c589a55735e23f0f9f6352bb1e0ea
rida300 <31077599+rida300@users.noreply.github.com>
2018-12-05 19:32:28 -0600

    Update StoredProcedures.sql

--- StoredProcedures.sql
+++ StoredProcedures.sql
@@ -1,3 +1,4 @@
+--procedures that create new entries in tables
 --Add movies:
 --1. Adding a movie to the moviesTable: title, year, rating, genre, star rating, duration
 drop procedure if exists AddMovie
@@ -7,17 +8,17 @@ CREATE PROCEDURE AddMovie
    @Year int,
    @duration int,
    @rating float,
-   @genreID nvarchar(100),
-   @Copies int
+   @genreID nvarchar(100)
+
 AS
-Insert group13proj.Movie(MovieTitle, ReleaseYear, Duration, Rating, GenreID, NumberOfCopies)
-values(@MovieName, @Year, @duration, @rating,@genreID, @Copies)
+Insert group13proj.Movie(MovieTitle, ReleaseYear, Duration, Rating, GenreID)
+values(@MovieName, @Year, @duration, @rating,@genreID)
 GO
 --example execution
-exec AddMovie 'Chucky 3', '2018', '123', '3.4', '12,13,3', '2'
-select * from group13proj.Movie
+exec AddMovie 'Chucky 3', '2018', '123', '3.4', '12,13,3'
+select * from group13proj.Movie M where M.MovieTitle='Chucky 3'
 
---Create account: 
+--Create account:
 --1. EMployee enters in the fname, lname, email, phone
 Drop Procedure if exists CreateAccount
 go
@@ -33,7 +34,31 @@ GO
 
 --example execution:
 EXEC CreateAccount'John', 'Doe', '(322) 343-4334' ,'john@idk.com';
-select* from group13proj.Account
+select* from group13proj.Account where FirstName='John' and LastName='Doe'
+
+--Modify account:
+--1. look up the account using the email and modify the optional parameters: fname, lname, email, phone
+drop procedure if exists ModifyAccount
+go
+create procedure ModifyAccount
+@OldEmail nvarchar(64),
+@NewEmail nvarchar(64),
+@FirstName nvarchar(256)= null,
+@LastName NVARCHAR(32) =null,
+@PhoneNumber NVARCHAR(15) =null
+AS
+update group13proj.Account
+set FirstName = Coalesce(@FirstName, FirstName),
+    LastName = Coalesce(@LastName, LastName),
+	PhoneNumber = Coalesce(@PhoneNumber, PhoneNumber),
+	Email = Coalesce(@NewEmail, email)
+where Email = @OldEmail
+go
+--example execution
+exec CreateAccount 'John', 'Doe','(373) 234-1333', 'john@idk.com'
+select * from group13proj.Account
+exec ModifyAccount 'john@idk.com', 'William'
+select * from group13proj.Account
 
 --Delete account:
 --1. ask for email and check if any rentals are due. if yes, dont delete, otherwise delete from database
@@ -44,73 +69,14 @@ CREATE PROCEDURE DeleteAccount
 AS
 Delete A
 from group13proj.Account A
-where A.Email = @Email
+where A.Email = @Email and A.AccountID not in(select R.AccountID from group13proj.Rental R )
+go
 --example execution
+select * from group13proj.Rental 
+select * from group13proj.Account where Email='john@idk.com'
 exec DeleteAccount 'john@idk.com'
-select * from group13proj.Account
+select * from group13proj.Account where Email='john@idk.com'
 
---Modify account:
---1. look up the account using the email and modify the optional parameters: fname, lname, email, phone
-drop procedure if exists ModifyAccount 
-go
-create procedure ModifyAccount
-@Email nvarchar(64),
-@FirstName nvarchar(256)= null,
-@LastName NVARCHAR(32) =null,
-@PhoneNumber NVARCHAR(15) =null
-AS 
-update group13proj.Account 
-set FirstName = Coalesce(@FirstName, FirstName),
-    LastName = Coalesce(@LastName, LastName),
-	PhoneNumber = Coalesce(@PhoneNumber, PhoneNumber)
-where Email = @Email
-go
---example execution
-exec CreateAccount 'John', 'Doe','(373) 234-1333', 'john@idk.com'
-select * from group13proj.Account
-exec ModifyAccount 'john@idk.com', 'William'
-select * from group13proj.Account
-
---Renting movies:
---1. all movies and their number of copies returned
-drop procedure if exists ShowMovies
-go
-create procedure ShowMovies
-as
-select M.MovieTitle, M.NumberOfCopies
-from group13proj.Movie M
-go
-exec ShowMovies
-
---function that returns the genreIds as a a table with each id on a separate row
-drop function if exists splitString
-go
-CREATE FUNCTION splitstring ( @stringToSplit VARCHAR(50) )
-RETURNS
- @returnList TABLE ([id] [nvarchar] (500))
-AS
-BEGIN
-
- DECLARE @name NVARCHAR(255)
- DECLARE @pos INT
-
- WHILE CHARINDEX(',', @stringToSplit) > 0
- BEGIN
-  SELECT @pos  = CHARINDEX(',', @stringToSplit)  
-  SELECT @name = SUBSTRING(@stringToSplit, 1, @pos-1)
-
-  INSERT INTO @returnList 
-  SELECT @name
-  SELECT @stringToSplit = SUBSTRING(@stringToSplit, @pos+1, LEN(@stringToSplit)-@pos)
- END
-
- INSERT INTO @returnList
- SELECT @stringToSplit
-
- RETURN
-END
-go
-select * from splitString('2,3,4,5')
 --procedure to convert the ids into genre names, each row in the table returned is a genre name
 drop procedure if exists genreId2Names
 go
@@ -119,10 +85,10 @@ create procedure genreId2Names
 @gNames nvarchar(max) output
 as
 select @gNames= coalesce(@gNames+',','')+isnull(temp.GenreName, '')
-from 
+from
 (
 select distinct G.GenreName
-from 
+from
 (
 	select id
 	from splitstring(@IdString)
@@ -138,10 +104,10 @@ select @outvar
 
 --This is the initial display for the rental window, only displays the name of the movie and the number of available copies we have
 drop procedure if exists initDispRental
-go 
+go
 
 create procedure initDispRental As
-Select M.MovieTitle, count(distinct i.InventoryID) as [Number of copies]
+Select M.MovieTitle, count(distinct i.InventoryID) as [copies]
 from group13proj.Inventory I
 	inner join group13proj.Movie M on m.MovieID = i.MovieID
 where i.Rented = 0
@@ -151,6 +117,187 @@ go
 exec initDispRental
 go
 
+
+
+--summary of account's rentals to be displayed (don't know if it works yet, need to finish rental)
+drop procedure if exists acctRentals
+go
+create procedure acctRentals
+@email nvarchar(64)
+as
+select m.MovieTitle, r.RentalDate, r.DueDate
+from group13proj.Rental R
+	inner join group13proj.Account A on A.Email=@email
+	inner join group13proj.Inventory I on i.InventoryID = r.InventoryID
+	inner join group13proj.Movie m on m.MovieID = i.MovieID
+where R.AccountID=A.AccountID
+go
+
+
+drop procedure if exists movieName2id
+go
+create procedure movieName2id
+@name nvarchar(255)
+as
+select M.MovieID
+from group13proj.Movie M where M.MovieTitle=@name
+go
+
+
+-- in process
+
+
+go
+
+--looks up a customers information to be displayed in the modify account window
+drop procedure if exists modAcctLookup
+go
+create procedure modAcctLookup
+@email Nvarchar(64)
+as
+select a.Email, a.PhoneNumber, a.FirstName, A.LastName
+from group13proj.Account A
+where a.Email = @email
+go
+
+exec modAcctLookup 'uncle@yahoo.com'
+go
+
+
+--used to check if a movie already exists in the database when being added.
+-- this currently doesn not work which means it cannot properly check the db, i need to sleep on it
+drop procedure if exists checkMovie
+go
+create procedure checkMovie
+@Title NvarChar(255),
+@Year int
+as
+Select m.MovieTitle
+from group13proj.Movie M
+where m.ReleaseYear = @year and m.MovieTitle=@Title
+go
+
+exec checkMovie 'Avatar', 2009
+
+
+drop procedure if exists AddInventory
+go
+create procedure AddInventory
+@Title NvarChar(255),
+@Year int
+as
+Insert group13proj.Inventory(MovieID)
+Select m.MovieID
+from group13proj.Movie M
+where m.MovieTitle = @Title and m.ReleaseYear = @Year
+go
+
+select *
+from group13proj.Movie i
+order by i.MovieID
+
+
+select *
+from group13proj.Inventory i
+where i.MovieID = 967
+
+select *
+from group13proj.Account
+   
+--filtering for the movie name is going to be done on the front end
+
+drop procedure if exists rentMovie
+go
+create procedure rentMovie
+@title NVarChar(255), @email NvarChar(64)
+as
+Insert group13proj.Rental(InventoryID, AccountID)
+Select Top(1)
+	    I.InventoryID, a.AccountID
+from group13proj.Movie M
+	inner join group13proj.Inventory I on I.MovieID = M.MovieID and i.Rented = 0
+	inner join group13proj.Account A on a.Email = @email
+where M.MovieTitle = @title;
+declare @rID int = Scope_Identity();
+select r.DueDate
+from group13proj.Rental r
+where r.RentalID = @rID
+go
+
+DROP TRIGGER IF EXISTS group13proj.tr_InsertingRentals;
+GO
+CREATE TRIGGER group13proj.tr_InsertingRentals ON group13proj.Rental
+AFTER insert
+AS
+update group13proj.Inventory
+set 
+rented = 1
+from group13proj.Inventory i
+where i.InventoryID = ( select top 1 i.InventoryID
+					  from inserted ins
+					  inner join group13proj.Inventory i on ins.InventoryID = i.InventoryID
+					  order by ins.RentalID desc
+					 )
+GO
+select * from group13proj.Inventory where MovieID =1
+
+exec rentMovie 'Tangled', 'uncle@yahoo.com'
+exec rentMovie 'Avatar', 'uncle@yahoo.com'
+exec rentMovie 'Avatar', 'uncle@yahoo.com'
+exec rentMovie 'Blackhat', 'kmiller@me.com'
+select * from group13proj.Rental
+select* from group13proj.Inventory where InventoryID=62
+exec movieName2id 'Blackhat'
+select*from group13proj.Inventory I where I.MovieID=606
+--if all copies of a movie have been rented and the employee tries to rent that movie, 
+--the procedure returns no rows. Need a message box to notify the eployee about this
+exec acctRentals 'uncle@yahoo.com'
+exec acctRentals 'kmiller@me.com'
+
+    
+drop procedure if exists returnRental
+go
+create procedure returnRental
+@movieTitle nvarchar(255),
+@email nvarchar(150)
+as
+update top (1) I
+set Rented = 0
+from group13proj.Inventory I
+inner join group13proj.Movie M on M.MovieTitle=@movieTitle
+where I.MovieID= M.MovieID;
+delete top (1) R
+from group13proj.Rental R
+inner join group13proj.Account A on A.Email=@email
+inner join group13proj.Movie M on M.MovieTitle=@movieTitle 
+inner join group13proj.Inventory I on I.MovieID=M.MovieID 
+where R.AccountID=A.AccountID and R.InventoryID=I.InventoryID
+go
+insert group13proj.Inventory(MovieID, Rented)
+values(1,1), (1,1);
+select* from group13proj.Inventory where MovieID=1
+select * from group13proj.Rental
+select movieID from group13proj.Movie where MovieTitle='Avatar'
+select* from group13proj.Inventory I where I.MovieID=1
+exec acctRentals 'uncle@yahoo.com'
+exec returnRental 'Avatar', 'uncle@yahoo.com'
+select* from group13proj.Inventory where InventoryID=62
+select*from group13proj.Rental
+
+--dont think we need this trigger
+DROP TRIGGER IF EXISTS group13proj.tr_DeletingRentals;
+GO
+CREATE TRIGGER group13proj.tr_DeletingRentals ON group13proj.Rental
+AFTER update
+AS
+delete R
+from group13proj.Rental R
+inner join group13proj.Inventory I on I.InventoryID = R.InventoryID
+GO
+
+
+/************************************************************************************************************************/
+--Filtering stuff
 --filtering for the movie name is going to be done on the front end
 drop procedure if exists filteredMovieTitle
 go
@@ -158,7 +305,7 @@ create procedure filteredMovieTitle
 @MovieTitle nvarchar(255) = '%'
 as
 select *
-from group13proj.Movie M 
+from group13proj.Movie M
 where M.MovieTitle like '%'+@MovieTitle+'%'
 go
 exec filteredMovieTitle 'vat'
@@ -256,27 +403,26 @@ go
 exec filteredYear @Operator='<',  @year=1999
 exec filteredYear @Operator='>',  @year=1998
 
---takes one genreId at a time				    
+--takes one genreId at a time
 drop procedure if exists filterGenres
 go
 create procedure filterGenres
 @id nvarchar(50)
 as
 select *
-from group13proj.Movie M 
+from group13proj.Movie M
 where M.GenreID like '%'+', '+@id+','+'%' or M.GenreID like '%'+ ', '+ @id or M.GenreID like @id+'%'
---where M.GenreID in( string_split (@listofIds, ',')as N on '%'+N.value+'%' like M.GenreID
 
 exec filterGenres '2'
 
 
---combined filers 
-				     
+--combined filers
+
 drop procedure if exists allFilters
 go
 create procedure allfilters
 @genreId nvarchar(50)='%',
-@duration int='%',
+@duration int=null,
 @doperator nchar(1)=null,
 @rating float='%',
 @roperator nchar(1)=null,
@@ -285,23 +431,29 @@ create procedure allfilters
 @movietitle nvarchar(255)='%'
 as
 select *
-from group13proj.Movie M 
-where M.GenreID like '%'+', '+@genreId+','+'%' and  ((@yoperator ='<' and M.ReleaseYear<@year) 
-or (@yoperator ='>' and M.ReleaseYear>@year)) and ((@doperator ='<' and M.Duration<@duration) 
-or (@doperator ='>' and M.Duration>@duration)) and ((@roperator ='<' and M.Rating<@rating) 
+from group13proj.Movie M
+where M.GenreID like '%'+', '+@genreId+','+'%' and  ((@yoperator ='<' and M.ReleaseYear<@year)
+or (@yoperator ='>' and M.ReleaseYear>@year)) and ((@doperator ='<' and M.Duration<@duration)
+or (@doperator ='>' and M.Duration>@duration)) and ((@roperator ='<' and M.Rating<@rating)
 or (@roperator ='>' and M.Rating>@rating)) and M.MovieTitle like '%'+@movietitle+'%'
 go
-exec allFilters 
+exec allFilters
 @genreId='14',
-@duration =120,
-@doperator ='>',
 @rating = 5.5,
 @roperator ='>',
 @year =2000,
 @yoperator ='>',
 @movietitle='vat'
+ 
 
- --filtering with both variables
+
+ select *
+ from group13proj.Rental
+ select*
+ from group13proj.Inventory
+
+
+ --**************************************************************************************************
     
 drop procedure if exists allFilters2
 go
@@ -323,39 +475,39 @@ create procedure allfilters2
 as
 select *
 from group13proj.Movie M 
-where M.GenreID like '%'+', '+@genreId+','+'%' and M.MovieTitle like '%'+@movietitle+'%'
+where M.GenreID like '%' + ', '+@genreId+','+'%' and M.MovieTitle like '%'+@movietitle+'%'
 intersect
 select *
 from group13proj.Movie M
-where (@yoperatorG ='>' and M.ReleaseYear>@yearG)
+where (@yoperatorG ='>' and M.ReleaseYear>@yearG)or(@yoperatorG=null and @yearG=null)
 intersect
 select *
 from group13proj.Movie M
-where  (@yoperatorL='<' and M.ReleaseYear<@yearL)
+where  ( @yoperatorL='<' and M.ReleaseYear<@yearL) or(@yoperatorL=null and @yearL=null)
 intersect
 select *
 from group13proj.Movie M
-where  @roperatorG ='>' and M.Rating>@ratingG
+where  @roperatorG ='>' and M.Rating>@ratingG or (@roperatorG=null and @ratingG=null)
 intersect
 select *
 from group13proj.Movie M
-where @roperatorL='<' and M.Rating<@ratingL
+where @roperatorL='<' and M.Rating<@ratingL or(@roperatorL=null and @ratingL=null)
 intersect
 select *
 from group13proj.Movie M
-where  @doperatorG ='>' and M.Duration>@durationG
+where  @doperatorG ='>' and M.Duration>@durationG or(@doperatorG=null and @durationG=null)
 intersect
 select *
 from group13proj.Movie M
-where @doperatorL='<' and M.Duration<@durationL
+where @doperatorL='<' and M.Duration<@durationL or(@doperatorL=null and @durationL=null)
 go
 exec allFilters2 
 @genreId='2',
-@durationG =120,
+@durationG =1,
 @durationL=200,
 @doperatorG ='>',
 @doperatorL='<',
-@ratingG = 3,
+@ratingG = 1,
 @ratingL = 10,
 @roperatorG ='>',
 @roperatorL ='<',
@@ -363,64 +515,28 @@ exec allFilters2
 @yoperatorG ='>'
 
 
-drop procedure if exists rentMovie
-go 
+--*********************************************************************************************************
 
-create procedure rentMovie
-@title NVarChar(255), @email NvarChar(64)
+
+drop procedure if exists allFilters2
+go
+create procedure allfilters2
+@durationG int=null,
+@durationL int=null,
+@doperatorG nchar(1)=null,
+@doperatorL nchar(1)=null
 as
-Insert group13proj.Rental(InventoryID, AccountID)
-Select Top(1)
-	    I.InventoryID, a.AccountID
-from group13proj.Movie M
-	inner join group13proj.Inventory I on I.MovieID = M.MovieID and i.Rented = 0
-	inner join group13proj.Account A on a.Email = @email
-where M.MovieTitle = @title;
+ IF (@durationG IS NOT NULL AND @doperatorG IS not NULL and @durationL IS NULL AND @doperatorL IS null )
+      select *
+	  from group13proj.Movie M where M.Duration>=@durationG
+  ELSE IF (@durationL IS NOT NULL AND @doperatorL IS not NULL and @durationG IS NULL AND @doperatorG IS null)
+        select*
+		from group13proj.Movie M where M.Duration<=@durationL
+  else if (@durationL IS NOT NULL AND @doperatorL IS not NULL and @durationG IS not NULL AND @doperatorG IS not null)
+  select * from group13proj.Movie M where M.Duration>=@durationG 
+  intersect 
+  select * from group13proj.Movie M where M.Duration<=@durationL
 
-
-declare @rID int = Scope_Identity();
-
-select r.DueDate
-from group13proj.Rental r
-where r.RentalID = @rID
-go
-
-
-
-
-
-
-
--- in process
-
-
-go
-
---looks up a customers information to be displayed in the modify account window
-drop procedure if exists modAcctLookup
-go
-create procedure modAcctLookup
-@email Nvarchar(64)
-as
-select a.Email, a.PhoneNumber, a.FirstName, A.LastName
-from group13proj.Account A
-where a.Email = @email
-go
-
-exec modAcctLookup 'uncle@yahoo.com'
-go
-
---summary of account's rentals to be displayed (don't know if it works yet, need to finish rental)
-drop procedure if exists acctRentals
-go
-create procedure acctRentals
-@email nvarchar(64)
-as
-select m.MovieTitle, r.RentalDate, r.DueDate
-from group13proj.Rental R
-	inner join ( select a.AccountID
-				 from group13proj.Account a
-				 where a.Email = @email) acct(acctID) on r.AccountID = acct.acctID
-	inner join group13proj.Inventory I on i.InventoryID = r.InventoryID
-	inner join group13proj.Movie m on m.MovieID = i.InventoryID
-go
+exec allFilters2
+@durationL=200,
+@doperatorL='<'
